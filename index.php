5cc8a9a2a920b94504868078a6c84b7aca28665e
Lorenzo Duarte <loredmuslera@gmail.com>
2026-06-10 10:07:26 -0300

    form.php

--- index.php
+++ index.php
@@ -15,26 +15,8 @@
         <h1>Bienvenidos a MateTech</h1>
         <p>Este es el contenido principal de la página de inicio.</p>
     </div>
-    
-        <form action="/action_page.php" class="login-caja">
-        <div class="mb-3 mt-3">
-            <label for="uname" class="form-label">Username:</label>
-            <input type="text" class="form-control" id="uname" placeholder="Enter username" name="uname">
-        </div>
-        <div class="mb-3 mt-3">
-            <label for="email" class="form-label">Email:</label>
-            <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
-        </div>
-        <div class="mb-3">
-    <label for="pwd" class="form-label">Password:</label>
-    <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
-  </div>
-  <div class="form-check mb-3">
-    <label class="form-check-label">
-      <input class="form-check-input" type="checkbox" name="remember"> Remember me
-    </label>
-  </div>
-  <button type="submit" class="btn">Submit</button>
-</form>
+    <div>
+    <?php include 'form.php'; ?>
+    </div>
 </body>
 </html>
