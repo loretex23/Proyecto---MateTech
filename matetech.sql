168ad2fed375daad6537c1cc6cd12a2a6cc2701a
Noorz <loredmuslera@gmail.com>
2026-07-10 09:43:54 -0300

    Base de datos

---
+++ matetech.sql
@@ -0,0 +1,86 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 09-07-2026 a las 13:17:04
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `matetech`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categorias`
+--
+
+CREATE TABLE `categorias` (
+  `CategoriaID` int(11) NOT NULL,
+  `NombreCategoria` varchar(20) NOT NULL,
+  `CantClubes` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `club`
+--
+
+CREATE TABLE `club` (
+  `ClubID` int(11) NOT NULL,
+  `NombreClub` int(11) NOT NULL,
+  `Usuario` int(11) NOT NULL,
+  `Contraseña_hash` varchar(18) NOT NULL DEFAULT 'NOT_NULL'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `jugadores`
+--
+
+CREATE TABLE `jugadores` (
+  `CI` int(8) NOT NULL,
+  `Nombre` varchar(30) NOT NULL,
+  `Apellido` varchar(30) NOT NULL,
+  `Posicion` varchar(20) NOT NULL,
+  `Fuerza` int(11) NOT NULL,
+  `ClubID` int(11) NOT NULL,
+  `CategoriaID` int(11) NOT NULL,
+  `FechaNacimiento` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indices de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`CategoriaID`);
+
+--
+-- Indices de la tabla `club`
+--
+ALTER TABLE `club`
+  ADD PRIMARY KEY (`ClubID`);
+
+--
+-- Indices de la tabla `jugadores`
+--
+ALTER TABLE `jugadores`
+  ADD PRIMARY KEY (`CI`);
+COMMIT;
+
+Alter table `´jugadores` add foreign key (`ClubID`) references `club`(`ClubID`);
+Alter table `jugadores` add foreign key (`CategoriaID`) references `categorias`(`CategoriaID`);
