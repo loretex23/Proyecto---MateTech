71170f782b45d4fff5c85768fca37ed6b601dd26
gab-dl <christiandeleondominguez@gmail.com>
2026-07-13 11:37:54 -0300

    Reorganiza estructura del proyecto

--- navbar_login.php
+++
@@ -1,8 +0,0 @@
-<nav class="navbar navbar-expand-lg" ID="navbar">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="index.php">
-            <img src="img/LogoFlow.png" alt="Avatar Logo" class="rounded-pill nav-logo" id="nav-logo"> 
-             <img src="img/LeagueFlow.png" alt="Avatar Logo" class="rounded-pill web-logo" id="web-logo">
-        </a>
-    </div>
-</nav>
